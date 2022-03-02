23762b7456732b6928d108d9502824a39a27b13f
Pietro Liguori <pietro_liguori@hotmail.com>
2022-03-02 13:10:16 -0300

    Many-to-many association with extra attributes PART 2

--- src/main/java/com/vili/course/config/TesteConfig.java
+++ src/main/java/com/vili/course/config/TesteConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.vili.course.entities.Category;
 import com.vili.course.entities.Order;
+import com.vili.course.entities.OrderItem;
 import com.vili.course.entities.Product;
 import com.vili.course.entities.User;
 import com.vili.course.entities.enums.OrderStatus;
 import com.vili.course.repositories.CategoryRepository;
+import com.vili.course.repositories.OrderItemRepository;
 import com.vili.course.repositories.OrderRepository;
 import com.vili.course.repositories.ProductRepository;
 import com.vili.course.repositories.UserRepository;
@@ -33,7 +35,10 @@ public class TesteConfig implements CommandLineRunner{
 	
 	@Autowired
 	private ProductRepository productRepository;
-
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
 		
@@ -68,6 +73,13 @@ public class TesteConfig implements CommandLineRunner{
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+		
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 
 	}
 	
--- src/main/java/com/vili/course/entities/OrderItem.java
+++ src/main/java/com/vili/course/entities/OrderItem.java
@@ -7,6 +7,7 @@ import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.vili.course.entities.pk.OrderItemPK;
 
 @Entity
@@ -16,11 +17,11 @@ public class OrderItem implements Serializable{
 	private static final long serialVersionUID = 1L;
 
 	@EmbeddedId
-	private OrderItemPK id;
-	
+	private OrderItemPK id = new OrderItemPK();
+
 	private Integer quantity;
 	private Double price;
-	
+
 	public OrderItem() {
 		
 	}
@@ -33,6 +34,7 @@ public class OrderItem implements Serializable{
 		this.price = price;
 	}
 	
+	@JsonIgnore
 	public Order getOrder() {
 		return id.getOrder();
 	}
